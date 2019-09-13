ec5b3cd152ef7c73f6dd3d0a39d814de882c7461
Chalk <chalkpe@gmail.com>
2019-09-13 21:31:20 +0900

    remove Proxy

--- src/database.js
+++ src/database.js
@@ -1,9 +1,14 @@
 import db from './assets/database.json'
 
-export default new Proxy(db, {
-  get: (jobs, id) => id in jobs ? jobs[id] :
-    new Proxy(jobs.find(j => j.id === id), {
-      get: (job, key) => key in job ? job[key] :
-        job.skills.pve.find(s => key === `${s.client}:${s.name}`)
-    })
-})
+// export default new Proxy(db, {
+//   get: (jobs, id) => id in jobs ? jobs[id] :
+//     new Proxy(jobs.find(j => j.id === id), {
+//       get: (job, key) => key in job ? job[key] :
+//         job.skills.pve.find(s => key === `${s.client}:${s.name}`)
+//     })
+// })
+
+// Chrome 45...
+
+const entries = job => job.skills.pve.map(s => ({ [`${s.client}:${s.name}`]: s }))
+export default Object.assign({}, ...db.map(job => ({ [job.id]: Object.assign({}, job, ...entries(job)) })))
